212f4ae7fffca0736017480208870ae40779f470
mzylka <m.zylka1311@gmail.com>
2023-09-12 12:34:50 +0200

    Removed the unique constraint from the email column of the User model; Added the condition to check if there is a duplicate username field during registration; Changed bg-color of the user page

--- app/models.py
+++ app/models.py
@@ -63,7 +63,7 @@ class Role(db.Model):
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(64), unique=True, index=True)
+    email = db.Column(db.String(64), index=True, default=None)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
